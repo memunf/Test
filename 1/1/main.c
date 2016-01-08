244f6927a867b22bf65444733aa507ebb2eef6fd
qianxiaomei <839505025@qq.com>
2016-01-08 16:59:56 +0800

    1

---
+++ 1/1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  1
+//
+//  Created by lkjy on 16/1/8.
+//  Copyright © 2016年 12计科. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
